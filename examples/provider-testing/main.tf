fd1fbf9146de76b2698bdef79f286a19e29b3fc0
swiftsudo <110781792+swiftsudo@users.noreply.github.com>
2024-09-04 22:15:29 -0700

    examples fixup

--- examples/provider-testing/main.tf
+++ examples/provider-testing/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-	  discord = {
-		  source = "registry.terraform.io/justarecord/discord"
-	  }
+    discord = {
+      source = "registry.terraform.io/justarecord/discord"
+    }
   }
 }
 
@@ -14,25 +14,25 @@ data "discord_guild" "merchtracker_dev" {
 
 data "discord_channel" "logs" {
   guild_id = data.discord_guild.merchtracker_dev.id
-  name = "logs"
+  name     = "logs"
 }
 
 resource "discord_channel" "testing" {
   guild_id = data.discord_guild.merchtracker_dev.id
-  name = "testing"
+  name     = "testing"
 }
 
 data "discord_role" "admins" {
   guild_id = data.discord_guild.merchtracker_dev.id
-  name = "admins"
+  name     = "admins"
 }
 
 resource "discord_role" "tf_testing" {
-  guild_id = data.discord_guild.merchtracker_dev.id
-  name = "tf_testing"
+  guild_id    = data.discord_guild.merchtracker_dev.id
+  name        = "tf_testing"
   mentionable = false
-  hoist = true
-  color = "#FFFFFF"
+  hoist       = true
+  color       = "#FFFFFF"
   permissions = [
     "READ_MESSAGE_HISTORY",
   ]
@@ -44,10 +44,10 @@ resource "discord_role" "tf_testing" {
 # }
 
 resource "discord_role" "testing" {
-  name = "testing"
-  guild_id = data.discord_guild.merchtracker_dev.id
-  color = "#1ABC9C"
-  hoist = true
+  name        = "testing"
+  guild_id    = data.discord_guild.merchtracker_dev.id
+  color       = "#1ABC9C"
+  hoist       = true
   mentionable = false
   permissions = [
     "VIEW_CHANNEL",
@@ -57,10 +57,10 @@ resource "discord_role" "testing" {
 }
 
 resource "discord_permissions" "tf_testing" {
-  guild_id = data.discord_guild.merchtracker_dev.id
+  guild_id   = data.discord_guild.merchtracker_dev.id
   channel_id = resource.discord_channel.testing.id
-  id = resource.discord_role.tf_testing.id
-  type = "role"
+  id         = resource.discord_role.tf_testing.id
+  type       = "role"
   allow = [
     "VIEW_CHANNEL",
   ]
@@ -69,10 +69,10 @@ resource "discord_permissions" "tf_testing" {
 }
 
 resource "discord_permissions" "testing" {
-  guild_id = data.discord_guild.merchtracker_dev.id
+  guild_id   = data.discord_guild.merchtracker_dev.id
   channel_id = resource.discord_channel.testing.id
-  id = resource.discord_role.testing.id
-  type = "role"
+  id         = resource.discord_role.testing.id
+  type       = "role"
   allow = [
     "VIEW_CHANNEL",
   ]
@@ -81,8 +81,8 @@ resource "discord_permissions" "testing" {
 }
 
 data "discord_permissions" "admins" {
-  guild_id = data.discord_guild.merchtracker_dev.id
+  guild_id   = data.discord_guild.merchtracker_dev.id
   channel_id = data.discord_channel.logs.id
-  id = data.discord_role.admins.id
-  type = "role"
+  id         = data.discord_role.admins.id
+  type       = "role"
 }
